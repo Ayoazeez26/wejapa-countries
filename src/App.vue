868edd6bd7a855a6e943e6e36dca289fc3cf9fdb
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-08-09 03:56:26 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Check />
+  </div>
+</template>
+
+<script>
+import Check from './components/Check.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Check
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Check.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Wejapa Countries</h1>
+
+    <b-form-select v-model="selected" :options="options" class="mb-3 mt-3">
+      <!-- This slot appears above the options from 'options' prop -->
+      <template v-slot:first>
+        <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
+      </template>
+
+      <!-- These options will appear after the ones from 'options' prop -->
+      <b-form-select-option value="C">Option C</b-form-select-option>
+      <b-form-select-option value="D">Option D</b-form-select-option>
+    </b-form-select>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selected: null,
+        options: [
+          { value: 'A', text: 'Option A (from options prop)' },
+          { value: 'B', text: 'Option B (from options prop)' }
+        ]
+      }
+    }
+  }
+</script>
